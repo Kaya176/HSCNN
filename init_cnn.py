5b1d5d16eb85d2e6b023458192ca1654ac81610b
kaya176 <diehssk@naver.com>
2021-06-08 18:36:01 +0900

    저장 안하고 commit한거 추가

--- init_cnn.py
+++ init_cnn.py
@@ -1,7 +1,11 @@
 import torch
 import torch.nn as nn
+from torch import optim
 import torch.nn.functional as F
+from torchtext import vocab
+from torchtext.data.field import LabelField
 from torchtext.data.iterator import pool
+import math
 
 class CNN_network(nn.Module):
 
@@ -20,8 +24,8 @@ class CNN_network(nn.Module):
 
         self.word_embedding = nn.Embedding(vocab_size,embedding_size)
         self.conv1 = nn.Conv2d(in_channels,out_channels,(kernel_size[0],embedding_size),stride,padding)
-        self.conv1 = nn.Conv2d(in_channels,out_channels,(kernel_size[1],embedding_size),stride,padding)
-        self.conv1 = nn.Conv2d(in_channels,out_channels,(kernel_size[2],embedding_size),stride,padding)
+        self.conv2 = nn.Conv2d(in_channels,out_channels,(kernel_size[1],embedding_size),stride,padding)
+        self.conv3 = nn.Conv2d(in_channels,out_channels,(kernel_size[2],embedding_size),stride,padding)
 
         self.dropout = nn.Dropout(keep_probab)
         self.linear = nn.Linear(len(kernel_size)*out_channels,1024)
@@ -42,7 +46,66 @@ class CNN_network(nn.Module):
         input_sent : sentences
         label_onehot : one-hot vector로 변환된 라벨(ex : [0,0,0,0,1] 등)
         '''
+        input_sentence = input_sent.transpose(1,0)
+        input = self.word_embedding(input_sentence)
+        input = self.dropout(input)
+        input = input.unsqueeze(1)
+
+        pool_out1 = self.conv_pool(input,self.conv1)
+        pool_out2 = self.conv_pool(input,self.conv2)
+        pool_out3 = self.conv_pool(input,self.conv3)
+        all_pool_out = torch.cat(pool_out1,pool_out2,pool_out3)
+        x = self.dropout(all_pool_out)
+        self.dropout1 = nn.Dropout(0.25)
+
+        x = self.linear(x)
+        cnn_out = self.label(x)
+
+        q_w = self.linear_onehot(label_onehot)
+        q_w = self.relu(q_w/math.sqrt(self.d))
+
+        return x,q_w,cnn_out
+kernel_size = [3,4,5]
+keep_prob = 0.25
+vocab_size = 999999#구해서 채워넣기
+embedding_size = 100
+d = 103
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = CNN_network(output_size = 1,in_channels= 1,out_channels=128,kernel_size=kernel_size,stride=1,padding = False,keep_probab=keep_prob,vocab_size=vocab_size,
+                    embedding_size=embedding_size,d=d).to(device)
+'''
+sia_model = HSCNN_network(output_size=1, in_channels=1, out_channels=128, kernel_size=kernel_size, stride=1, padding=0,
+                    keep_probab=keep_probab, vocab_size=vocab_size, embedding_size=embedding_size,d = d)
+'''
+optimizer = optim.Adam(model.parameters(),lr = 1e-3)
+
+def train(train_data,model):
+    loss_fn = torch.nn.BCEWithLogitsLoss()
+    model.train()
+    train_loss = 0
+    count = 0 
+    epochs = 100
+    for epoch in range(epochs):
+        
+        for batch_idx,batch in enumerate(train_batch_data):
+            text = batch.text1
+            label = batch.label
+            label1 = batch.onehot1
+            label = label.unsqueeze(1)
+            label = label.clone().detach().requires_grad_(True)
+
+            text.to(device)
+            label.to(device)
+            label1.to(device)
+            
+            optimizer.zero_grad()
+            x,q_w,out = model(data)
+            loss = loss_fn(out,label1)
+            count += 1
+            train_loss += loss
+            loss.backward()
+            optimizer.step()
+        print(f"Epoch : {epoch} \t Train  Loss : {train_loss/count}")
 
 if __name__ == '__main__':
-    model  = CNN_network(???)
-    #구현해야할것 : train & checkpoint
+    train()
--- load_data.py
+++ load_data.py
@@ -2,9 +2,71 @@
 '''
 TabularDataset : Defines a Dataset of columns stored in CSV, TSV, or JSON format.
 BucketIterator : Defines an iterator that batches examples of similar lengths together.
+fasttext embedding을 사용하기 위해서는 pre-train 모델과 내가 직접 데이터로 훈련한 model 둘 다 필요한데,
+pre-trained model : wiki.en.vec 을사용하고,
+fine-tuned model은 저장을 vec 파일로 한 뒤, torchtext에서 불러와야 하는것으로 보인다.
+-> gensim에서 save하는 함수는 bin파일로 저장하는 형태
 
+나중에 알아보도록 하고, 우선 pre-train된 파일을 이용하여 진행하는것으로 하고,
+밤에 다른 방법을 이용하여 fine-tuned된 파일을 torchtext에서 사용하는 방법을 이용해보자.
 '''
+from torch._C import device
+from torch.nn.modules.container import Sequential
+import torchtext
+from torchtext import data
+import torch
 
+def make_onehot(labels):
+    
 def load_data():
 
-    
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    TEXT = data.Field(fix_length = 500)
+    LABEL = data.Field(Sequential =False,is_target = True,use_vobcab = False,dtype = torch.float64)
+    #ONEHOT = data.Field(Sequential = False, is_target = True, use_vocab = False, dtype = torch.float64)
+
+    field = {'text': ('text', TEXT),'label': ('label', LABEL)}
+
+    train_pairs= data.TabularDataset.splits(
+        train='tmc2007-train.csv',
+        format='csv',
+        fields=field,
+        skip_header = True
+    )
+    test_pairs = data.TabularDataset.split(
+        test = 'tmc2007-test.csv',
+        format = 'csv',
+        fileds = field,
+        skip_header = True
+    )
+
+    #Embedding vectors - Pretrained
+    vector = torchtext.vocab.Vectors(name = 'wiki.en.vec')
+    #Embedding vecotrs - Custom vector
+    #vector = torchtext.vocab.Vectors(name = "MyCustomEmbeddingVectors") 
+
+    TEXT.build_vocab(train_pairs,vectors = vector)
+
+    print("length of Text Vocab : ",len(TEXT.vocab))
+    print("Dim of Text : ",TEXT.vocab.vectors.size()[1])
+
+    train_pair_batch = data.BucketIterator(
+        dataset = train_pairs,
+        sort = False,
+        batch_size= 100,
+        repeat = False,
+        shuffle= True,
+        device = device
+    )
+
+    test_pair_batch = data.BucketIterator(
+        dataset = train_pairs,
+        sort = False,
+        batch_size= 100,
+        repeat = False,
+        shuffle= True,
+        device = device
+    )
+
+    return train_pair_batch,test_pair_batch
